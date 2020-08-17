9c2e40a2817133e75234b718e456f1daf974726e
nick migel <nickmigel2@gmail.com>
2020-08-17 16:15:41 -0400

    print8.ls8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,15 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +21,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +50,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +62,43 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        value = self.ram[address]
+        return value
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+
+        while running:
+            cmd = self.ram[self.pc]
+
+            if cmd == 0b00000001:
+                running = False
+            if cmd == 0b10000010:
+                indx = self.ram[self.pc+1]
+                value = self.ram[self.pc+2]
+                self.reg[indx] = value
+                self.pc += 2
+            if cmd == 0b01000111:
+                indx = self.ram[self.pc+1]
+                print(self.reg[indx])
+                self.pc += 1
+            self.pc += 1
+
+
+# testing = [
+#     # From print8.ls8
+#     0b10000010,  # LDI R0,8
+#     0b00000000,
+#     0b00001000,
+#     0b01000111,  # PRN R0
+#     0b00000000,
+#     0b00000001,  # HLT
+# ]
+
+# for item in testing:
+#     print(item)
